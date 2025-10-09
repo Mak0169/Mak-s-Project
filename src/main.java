3448ebce1f43c7a55f5cd6da45b3b78b21c05491
MakoMR <mak.gumpf@gmail.com>
2025-10-09 13:27:59 -0400

    Added to methods that compute two functions
    The first method computes the forward propagation by computing the weighted some of each node.
    
    Then the second method uses the sigmoid function which I read and found the function for in the link in the javadoc comments above the method.
    This method should return a number between 0 and 1. There are some good visual representation of what this looks like in a neural network.
    
    Overall this main class returns the weighted sum and output after the sigmoid function.

--- src/main.java
+++ src/main.java
@@ -1,6 +1,12 @@
 import components.sequence.Sequence;
 import components.sequence.Sequence1L;
 
+/**
+ * This is the link to where I got the formula for this project.
+ * https://www.geeksforgeeks.org/artificial-intelligence/artificial-neural
+ * -networks-and-its-applications/
+ */
+
 public class main {
     public static void main(String[] args) {
         Sequence<Double> inputs = new Sequence1L<>();
@@ -20,5 +26,40 @@ public class main {
         weights.add(weights.length(), w1);
         weights.add(weights.length(), w2);
         weights.add(weights.length(), w3);
+
+        double bias = 0.1;
+
+        double z = computeWeightedSum(inputs, weights) + bias;
+        double output = sigmoidFunction(z);
+
+        System.out.println("Weighted sum: " + z);
+        System.out.println("Output after sigmoid function: " + output);
+    }
+
+    private static double computeWeightedSum(Sequence<Double> inputs,
+            Sequence<Double> weights) {
+        double sum = 0.0;
+
+        /*
+         * This computes the summation of each input vector multiplied by its
+         * corresponding weight vector.
+         *
+         * This is the link to where I got the formula for this function.
+         * https://www.geeksforgeeks.org/machine-learning/backpropagation-in-
+         * neural-network/
+         */
+        for (int i = 0; i < inputs.length(); i++) {
+            sum += inputs.entry(i) * weights.entry(i);
+        }
+        return sum;
+    }
+
+    /*
+     * This is a link going to where I got the function for this method.
+     * https://www.geeksforgeeks.org/machine-learning/derivative-of-the-sigmoid-
+     * function/
+     */
+    private static double sigmoidFunction(double x) {
+        return 1.0 / (1.0 + Math.exp(-x));
     }
 }
