b4a08a0b2355868f45f6e0922d3bd9044aa98c0a
MakoMR <mak.gumpf@gmail.com>
2025-11-07 13:32:35 -0500

    Had to change train method
    Now I am having a bunch of issues with the abstract class so I have to figure why things are not working properly.

--- src/Neuron1.java
+++ src/Neuron1.java
@@ -66,48 +66,6 @@ public final class Neuron1 extends NeuronSecondary {
         return 1.0 / (1.0 + Math.exp(-x));
     }
 
-    /**
-     * This method will return a new Bias after updating the weights in-place.
-     * https://www.geeksforgeeks.org/machine-learning/backpropagation-in-neural-network/
-     * This link above is where I got the formulas for calculating the gradients
-     * and solve for delta. How to calculate the error and derivative.
-     *
-     * @param inputs
-     *            the input sequence
-     * @param weights
-     *            the weight sequence
-     * @param bias
-     *            the bias
-     * @param target
-     *            the target value
-     * @param learningRate
-     *            the learning rate
-     * @return the new bias
-     * @updates weights
-     */
-    @Override
-    public double train(Sequence<Double> inputs, Sequence<Double> weights,
-            double bias, double target, double learningRate) {
-        double z = this.computeWeightedSum(inputs);
-        double output = sigmoidFunction(z);
-
-        /*
-         * This will compute the error, derivative, and delta for the
-         * backpropagation
-         */
-        double err = output - target;
-        double derivative = output * (1 - output);
-        double delta = err * derivative;
-
-        for (int i = 0; i < weights.length(); i++) {
-            double newWeight = weights.entry(i);
-            double gradient = delta * inputs.entry(i);
-            weights.replaceEntry(i, newWeight - learningRate * gradient);
-        }
-        double newBias = bias - learningRate * delta;
-        return newBias;
-    }
-
     @Override
     public int size() {
         return this.w.length();
--- src/NeuronSecondary.java
+++ src/NeuronSecondary.java
@@ -1,3 +1,5 @@
+import components.sequence.Sequence;
+
 public abstract class NeuronSecondary implements Neuron {
     @Override
     public void clear() {
@@ -17,5 +19,44 @@ public abstract class NeuronSecondary implements Neuron {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException(
                 "Unimplemented method 'transferFrom'");
+    }
+
+    /**
+     * This method will return a new Bias after updating the weights in-place.
+     * https://www.geeksforgeeks.org/machine-learning/backpropagation-in-neural-network/
+     * This link above is where I got the formulas for calculating the gradients
+     * and solve for delta. How to calculate the error and derivative.
+     *
+     * @param inputs
+     *            the input sequence
+     * @param target
+     *            the target value
+     * @param learningRate
+     *            the learning rate
+     * @return the new bias
+     * @updates weights
+     */
+    @Override
+    public double train(Sequence<Double> inputs, double target,
+            double learningRate) {
+        double z = this.computeWeightedSum(inputs);
+        double output = sigmoidFunction(z);
+        final double num = 0.5;
+
+        /*
+         * This will compute the error, derivative, and delta for the
+         * backpropagation
+         */
+        double err = output - target;
+        double derivative = output * (1.0 - output);
+        double delta = err * derivative;
+
+        for (int i = 0; i < this.size(); i++) {
+            double newWeight = this.weight(i);
+            double gradient = delta * inputs.entry(i);
+            this.setWeights(i, newWeight - learningRate * gradient);
+        }
+        this.setBias(this.bias() - learningRate * delta);
+        return num * (output - target) * (output - target);
     }
 }
