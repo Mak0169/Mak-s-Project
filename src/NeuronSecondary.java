0a648fcc4df0cdcf62c792ed69068dbff401d0b3
MakoMR <mak.gumpf@gmail.com>
2025-11-21 17:50:01 -0500

    Finished clear. newInstance, transferFrom
    This should be hopefully the correct implementation.

--- src/NeuronSecondary.java
+++ src/NeuronSecondary.java
@@ -60,21 +60,29 @@ public abstract class NeuronSecondary implements Neuron {
 
     @Override
     public void clear() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'clear'");
+        for (int i = 0; i < this.size(); i++) {
+            this.setWeights(i, 0.0);
+        }
+        this.setBias(0.0);
     }
 
     @Override
     public NeuronKernel newInstance() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException(
-                "Unimplemented method 'newInstance'");
+        return new Neuron1(this.size());
     }
 
     @Override
     public void transferFrom(NeuronKernel source) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException(
-                "Unimplemented method 'transferFrom'");
+        if (source == this) {
+            throw new IllegalArgumentException("Cannot transfer self");
+        }
+        if (source.size() != this.size()) {
+            throw new IllegalArgumentException("sizes are mismatched");
+        }
+        for (int i = 0; i < this.size(); i++) {
+            this.setWeights(i, source.weight(i));
+        }
+        this.setBias(source.bias());
+        source.clear();
     }
 }
